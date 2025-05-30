bcdad1b12cf9349b3b9a8c02b39cf27fdf97cd60
MOHAMED AIT TAJANTE <mhdjant607@gmail.com>
2025-05-30 12:57:28 +0100

    update header, closing toggle-menu onclick on somhwere on the screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaSun, FaMoon, FaBars } from "react-icons/fa";
 import "./Header.css";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const navRef = useRef(null);
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+  useEffect(() => {
+    const handlclickOutside = (event) => {
+      if (menuOpen && navRef.current && !navRef.current.contains(event.target))
+        setMenuOpen(false);
+  }
+  document.addEventListener("mousedown", handlclickOutside);
+  return () => {
+    document.removeEventListener("mousedown", handlclickOutside);
+  };
+  }, [menuOpen]);
+
   return (
+    <>
     <header className="header" id="header">
-      <div className="logo">
+      <a href="#top" className="logo" onClick={() => setMenuOpen(false)}>
       <span className="logo-part1">BOU</span>
       <span className="logo-part2">GHANIM</span>
-    </div>
-
-      {/* <div className="logo"> <video autoPlay loop muted src="/logov.mp4"></video>
-    </div> */}
+      </a>
       <div className="header-controls">
         {!menuOpen && (
           <button className="menu-toggle" onClick={toggleMenu}>
@@ -24,19 +34,20 @@ function Header() {
         )}
       </div>
 
-      <nav className={`nav ${menuOpen ? "open" : ""}`}>
+    </header>
+      <nav ref={navRef} className={`nav ${menuOpen ? "open" : ""}`}>
         {menuOpen && (
           <button className="close-menu" onClick={() => setMenuOpen(false)}>
             ✖
           </button>
         )}
-        <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
         <a href="#teams" onClick={() => setMenuOpen(false)}>Teams</a>
         <a href="#schedule" onClick={() => setMenuOpen(false)}>Schedule</a>
         <a href="#results" onClick={() => setMenuOpen(false)}>Results</a>
         <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
+        <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
       </nav>
-    </header>
+        </>
   );
 }
 
